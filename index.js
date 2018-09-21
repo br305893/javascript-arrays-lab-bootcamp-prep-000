f000427e38242f4bbd1bb1c7039ff353068dd2a4
Ben Rosner <br305893@gmail.com>
2018-09-21 02:39:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-kittens.splice(kittens.length - 1, 0, name)
+kittens.splice(kittens.length, 0, name)
 return kittens
 }
