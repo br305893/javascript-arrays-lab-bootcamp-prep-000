240ab9bf375f514628f4b24b69fb9069b571b61a
Ben Rosner <br305893@gmail.com>
2018-09-21 03:02:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.slice(kittens.length)
+  return kittens.slice(kittens.length - 0)
 }
 
 function removeFirstKitten(){
