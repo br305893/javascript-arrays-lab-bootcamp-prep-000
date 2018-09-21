4f4768225d254699018f150f1439a9534de7cbfe
Ben Rosner <br305893@gmail.com>
2018-09-21 03:29:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function appendKitten(name){
 }
 
 function prependKitten(name){
-  var newKittenArray = kittens.splice(0, 0, name)
+  var newKittenArray = kittens.slice(0, 0, name)
   return newKittenArray
 }
 
